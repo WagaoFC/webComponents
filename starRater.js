c6ac46f4dc44eb1441b279e32384c1ca8e731f77
WagaoFC <wagnerferreira_07@hotmail.com>
2021-08-29 19:41:11 -0300

    first commit

---
+++ starRater.js
@@ -0,0 +1,53 @@
+class starRater extends HTMLElement {
+    constructor() {
+        super()
+
+        this.build()
+    }
+
+    build() {
+        const shadow = this.attachShadow({ mode: 'open' })
+    
+        shadow.appendChild(this.styles())
+
+        const rater = this.createRater()
+        const stars = this.createStars()
+
+        stars.forEach(star => rater.appendChild(star))
+
+        shadow.appendChild(rater)
+    }
+
+    createRater() {
+        const rater = document.createElement('div')
+        rater.classList.add('star-rater')
+        return rater
+    }
+
+    createStars() {
+        const createStar = (_, id) => {
+            const star = document.createElement('span')
+            star.classList.add('star')
+            star.setAttribute('data-value', Number(id) + 1)
+            star.innerHTML = '&#9733;'
+            return star
+        }
+
+        return Array.from({ length: 5 }, createStar)
+    }
+    
+    styles() {
+        const style = document.createElement('style')
+        style.textContent = `
+            .star-rater {
+                font-size: 10rem;
+            }
+        `
+    
+        return style
+    }
+}
+
+
+
+customElements.define('star-rater', starRater)
